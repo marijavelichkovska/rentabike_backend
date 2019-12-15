2e6a2c98c71339a13b213172f817c7eddcf6da7a
marijavelichkovska <marija.velickovska.55@gmail.com>
2019-12-15 18:33:28 +0100

    commited

---
+++ Controllers/TripController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication20.Models;
+using WebApplication20.VM;
+
+namespace WebApplication20.Controllers
+{
+    public class TripController : ApiController
+    {
+        TripDatabase DB = new TripDatabase();
+        [HttpPost]
+        [Route("api/trip/start")]
+        public void startTrip (int userId,int bikeID)
+        {
+            TripTbl newTrip = new TripTbl();
+            UserTbl user = DB.UserTbls.Find(userId);
+            newTrip.startLongitude = user.longitude;
+            newTrip.startLatitude = user.latitude;
+            newTrip.status ="in-progress";
+            newTrip.startTime = DateTime.Now;
+            BikeTbl bike = DB.BikeTbls.Find(bikeID);
+            if (bike.status.Equals("unlocked"))
+            {
+
+                newTrip.BikeTbl = bike;
+                newTrip.Bike = bikeID;
+                newTrip.UserID = userId;
+                DB.TripTbls.Add(newTrip);
+                bike.status = "occupied";
+                DB.SaveChanges();
+            }
+
+
+
+        }
+        [HttpPost]
+        [Route("api/trip/end")]
+        public TripCost endTrip(int TripID)
+        {
+            TripTbl trip = DB.TripTbls.Find(TripID);
+            trip.status = "finished_not_paid";
+            trip.endTime = DateTime.Now;
+            BikeTbl bike = DB.BikeTbls.Find(trip.Bike);
+            trip.endLongitude = bike.longitude;
+            trip.endLatitude = bike.latitude;
+            TimeSpan span = (TimeSpan)(trip.endTime - trip.startTime);
+            double minutes = span.TotalMinutes;
+            int finalCost = (int)(minutes * 0.5);
+            trip.cost = finalCost;
+            TripCost tripcost = new TripCost();
+            tripcost.cost = finalCost;
+            tripcost.TripID = TripID;
+            DB.SaveChanges();
+            return tripcost;
+           
+
+        }
+        [HttpPost]
+        [Route("api/trip/makePayment")]
+        public void makePayment (int tripId, int cost,int userId)
+        {
+            UserTbl user = DB.UserTbls.Find(userId);
+            string card = user.cardNum;
+            PaymentsTbl payment = new PaymentsTbl();
+            payment.cardNum = card;
+            payment.cost = cost;
+            payment.tripID = tripId;
+            DB.PaymentsTbls.Add(payment);
+            TripTbl trip = DB.TripTbls.Find(tripId);
+            trip.status = "finished_paid";
+            BikeTbl bike = DB.BikeTbls.Find(trip.Bike);
+            bike.status = "locked";
+            DB.SaveChanges();
+        }
+
+    }
+}
---
+++ VM/TripCost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication20.VM
+{
+    public class TripCost
+    {
+        public int TripID { set; get; }
+        public int cost { set; get; }
+
+    }
+}
