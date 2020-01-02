1e196c1db8842916c27a7a30d400eef99ede9c6d
marijavelichkovska <marija.velickovska.55@gmail.com>
2020-01-02 16:56:50 +0100

    -- Code refactoring w/ Strategy Pattern

---
+++ Controllers/Payment/DiscountMemberPricing.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication20.Controllers.Payment
+{
+    public class DiscountMemberPricing
+    {
+    }
+}
--- Controllers/TripController.cs
+++ Controllers/TripController.cs
@@ -12,25 +12,36 @@ namespace WebApplication20.Controllers
     public class TripController : ApiController
     {
         rentbikeEntities DB = new rentbikeEntities();
+
         [HttpPost]
         [Route("api/trip/start")]
+
+        //  The trip is started with unique userId and bikeId as parameters 
+
         public void startTrip (int userId,int bikeID)
         {
             TripTbl newTrip = new TripTbl();
+            //Retrieve from database user and bike with unique values of ids
             UserTbl user = DB.UserTbls.Find(userId);
+            BikeTbl bike = DB.BikeTbls.Find(bikeID);
+
+            // Set the start coordinates of the trip to be the same as the user's 
             newTrip.startLongitude = user.longitude;
             newTrip.startLatitude = user.latitude;
-            newTrip.status ="in-progress";
             newTrip.startTime = DateTime.Now;
-            BikeTbl bike = DB.BikeTbls.Find(bikeID);
+            // Change the status of the trip 
+            newTrip.status ="in-progress";
+           
+            // Only unlocked bikes are available for a trip
             if (bike.status.Equals("unlocked"))
             {
-
                 newTrip.BikeTbl = bike;
                 newTrip.Bike = bikeID;
                 newTrip.UserID = userId;
                 DB.TripTbls.Add(newTrip);
+                // Change the status of the bike from unlocked to occupied when the trip is started 
                 bike.status = "occupied";
+
                 DB.SaveChanges();
             }
 
@@ -42,19 +53,27 @@ namespace WebApplication20.Controllers
         public TripCost endTrip(int TripID)
         {
             TripTbl trip = DB.TripTbls.Find(TripID);
+            // Change the status of the trip to be in a mid-state before the payment
             trip.status = "finished_not_paid";
             trip.endTime = DateTime.Now;
             BikeTbl bike = DB.BikeTbls.Find(trip.Bike);
+
+            // Set the end coordinates to be the same as bikes coordinates at the end of trip
             trip.endLongitude = bike.longitude;
             trip.endLatitude = bike.latitude;
+
+            // Calculates the time of the trip in minutes
             TimeSpan span = (TimeSpan)(trip.endTime - trip.startTime);
             double minutes = span.TotalMinutes;
+
+
             int finalCost = (int)(minutes * 0.5);
             trip.cost = finalCost;
             TripCost tripcost = new TripCost();
             tripcost.cost = finalCost;
             tripcost.TripID = TripID;
             DB.SaveChanges();
+            // Returns an object of a pair of tripId and cost of trip
             return tripcost;
            
 
@@ -71,8 +90,10 @@ namespace WebApplication20.Controllers
             payment.tripID = tripId;
             DB.PaymentsTbls.Add(payment);
             TripTbl trip = DB.TripTbls.Find(tripId);
+            // Changes the status from "finished-not-paid" to "finished-paid"
             trip.status = "finished_paid";
             BikeTbl bike = DB.BikeTbls.Find(trip.Bike);
+            // Change the status back to lock when payment is done
             bike.status = "locked";
             DB.SaveChanges();
         }
